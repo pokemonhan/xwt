ef6b3915e82ac72833a2032191b29d673f59ab6b
LingPh <953909858@qq.com>
2019-09-23 21:09:24 +0800

    前台-代理中心 给下级转账需要验证资金密码和银行卡号

--- app/Http/Requests/Frontend/UserAgentCenter/UserAgentCenterTransferToChildRequest.php
+++ app/Http/Requests/Frontend/UserAgentCenter/UserAgentCenterTransferToChildRequest.php
@@ -27,13 +27,24 @@ class UserAgentCenterTransferToChildRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'user_id' => 'required|integer|exists:frontend_users,id',
-            'amount' => 'required|numeric',
+            'user_id' => 'required|integer|exists:frontend_users,id', //下级的用户id
+            'amount' => 'required|numeric', //转账金额
+            'fund_password' => ['required', 'string', 'between:6,18', 'regex:/^(?=.*[a-zA-Z]+)(?=.*[0-9]+)[a-zA-Z0-9]+$/'], //资金密码
+            'bank_card_id' => 'required|integer', // 银行卡id
+            'bank_card_number' => ['required', 'regex:/^(\d{15}|\d{16}|\d{19})$/'], //银行卡号
         ];
     }
 
-    /*public function messages()
+    /**
+     * 自定义返回信息
+     * @return array
+     */
+    public function messages()
     {
-
-    }*/
+        return [
+            'fund_password.between' => '资金密码长度必须是6到18位',
+            'fund_password.regex' => '资金密码必须是字母+数字组合，不能有特殊字符',
+            'bank_card_number.regex' => '银行卡号格式不正确',
+        ];
+    }
 }
--- app/Http/SingleActions/Frontend/User/AgentCenter/UserAgentCenterTransferToChildAction.php
+++ app/Http/SingleActions/Frontend/User/AgentCenter/UserAgentCenterTransferToChildAction.php
@@ -4,9 +4,11 @@ namespace App\Http\SingleActions\Frontend\User\AgentCenter;
 
 use App\Http\Controllers\FrontendApi\FrontendApiMainController;
 use App\Models\User\FrontendUser;
-use Illuminate\Http\JsonResponse;
+use App\Models\User\Fund\FrontendUsersBankCard;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * 转账给下级
@@ -34,29 +36,19 @@ class UserAgentCenterTransferToChildAction
      */
     public function execute(FrontendApiMainController $contll, array $inputDatas): JsonResponse
     {
-        $child = $this->model::find($inputDatas['user_id']);
-        if ($child === null) {
-            return $contll->msgOut(false, [], '100604');
-        }
-        $user = $contll->partnerUser;
-        $userRidArr = explode('|', $child->rid);
-        if (!in_array($user->id, $userRidArr)) {
-            return $contll->msgOut(false, [], '100605');
-        }
-        if ($child->id === $user->id) {
-            return $contll->msgOut(false, [], '100607');
-        }
-        $userAccount = $user->account;
-        if ($userAccount === null) {
-            return $contll->msgOut(false, [], '100606');
-        }
-        $childAccount = $child->account;
-        if ($childAccount === null) {
-            return $contll->msgOut(false, [], '100608');
+        $user = (object) [];
+        $child = (object) [];
+        $userAccount = null;
+        $childAccount = null;
+
+        $verification = $this->verification($contll, $inputDatas, $user, $child, $userAccount, $childAccount);
+        if ($verification['success'] === false) {
+            return $contll->msgOut(false, [], $verification['code']);
         }
 
         DB::beginTransaction();
         try {
+            //扣除上级用户金额
             $params = [
                 'user_id' => $user->id,
                 'to_id' => $child->id,
@@ -67,7 +59,7 @@ class UserAgentCenterTransferToChildAction
                 DB::rollback();
                 return $contll->msgOut(false, [], '', $result);
             }
-
+            //增加下级用户金额
             $params = [
                 'user_id' => $child->id,
                 'from_id' => $user->id,
@@ -78,10 +70,82 @@ class UserAgentCenterTransferToChildAction
                 DB::rollback();
                 return $contll->msgOut(false, [], '', $result);
             }
+
             DB::commit();
             return $contll->msgOut(true, [], '100609');
         } catch (Exception $e) {
             return $contll->msgOut(false, [], $e->getCode(), $e->getMessage());
         }
+    }
+
+    /**
+     * 验证信息
+     * @param FrontendApiMainController $contll       Controller.
+     * @param array                     $inputDatas   传递的参数.
+     * @param object                    $user         上级用户.
+     * @param object                    $child        下级用户.
+     * @param mixed                     $userAccount  上级用户的Account表.
+     * @param mixed                     $childAccount 下级用户的Account表.
+     * @return array
+     */
+    private function verification(
+        FrontendApiMainController $contll,
+        array $inputDatas,
+        object &$user,
+        object &$child,
+        &$userAccount,
+        &$childAccount
+    ) {
+        //接受转账的用户
+        $child = $this->model::find($inputDatas['user_id']);
+        if ($child === null) {
+            return $this->returnDataArr(false, '100604');
+        }
+        //是否是下级
+        $user = $contll->partnerUser;
+        $userRidArr = explode('|', $child->rid);
+        if (!in_array($user->id, $userRidArr)) {
+            return $this->returnDataArr(false, '100605');
+        }
+        //不能自己给自己转账
+        if ($child->id === $user->id) {
+            return $this->returnDataArr(false, '100607');
+        }
+        //用户的Account表
+        $userAccount = $user->account;
+        if ($userAccount === null) {
+            return $this->returnDataArr(false, '100606');
+        }
+        //接受转账的用户Account表
+        $childAccount = $child->account;
+        if ($childAccount === null) {
+            return $this->returnDataArr(false, '100608');
+        }
+        //资金密码
+        if (!Hash::check($inputDatas['fund_password'], $contll->partnerUser->fund_password)) {
+            return $this->returnDataArr(false, '100610');
+        }
+        //银行卡验证
+        $bankCard = FrontendUsersBankCard::where([
+            ['id', $inputDatas['bank_card_id']],
+            ['user_id', $contll->partnerUser->id],
+        ])->first();
+        if ($bankCard === null) {
+            return $this->returnDataArr(false, '100611');
+        }
+        if ((string) $inputDatas['bank_card_number'] !== $bankCard->card_number) {
+            return $this->returnDataArr(false, '100612');
+        }
+    }
+
+    /**
+     * 拼装验证返回信息arr
+     * @param  boolean $success 状态.
+     * @param  string  $code    返回信息编码.
+     * @return array
+     */
+    private function returnDataArr(bool $success, string $code)
+    {
+        return ['success' => $success, 'code' => $code];
     }
 }

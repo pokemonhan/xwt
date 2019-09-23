6bbf3c5f83cd84b68187fd81f85c73d8942ce072
pengpeng <vonly1024@gmail.com>
2019-09-23 19:32:38 +0800

    娱乐城入口

--- app/Models/Casino/CasinoGameList.php
+++ app/Models/Casino/CasinoGameList.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Models\Casino;
 
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class CasinoGameList
@@ -10,85 +10,23 @@ use Illuminate\Support\Facades\Validator;
 class CasinoGameList extends BaseCasinoModel
 {
     /**
-     * @var array
-     */
-    public $rules = [
-        'main_game_plat_code'           => 'required',
-        'cn_name'                       => 'required|min:2｜max:64',
-        'en_name'                       => 'required|min:2｜max:64',
-    ];
-
-    /**
-     * @param array   $c        Data.
-     * @param integer $pageSize PageSize.
-     * @return array
-     */
-    public static function getList(array $c, int $pageSize = 20)
-    {
-        $query = self::orderBy('id', 'desc');
-
-        $currentPage    = isset($c['page_index']) ? intval($c['page_index']) : 1;
-        $pageSize       = isset($c['page_size']) ? intval($c['page_size']) : $pageSize;
-
-        $offset         = ($currentPage - 1) * $pageSize;
-
-        $total  = $query->count();
-        $data  = $query->skip($offset)->take($pageSize)->get();
-
-
-        return ['data' => $data, 'total' => $total, 'currentPage' => $currentPage, 'totalPage' => intval(ceil($total / $pageSize))];
-    }
-
-    /**
      * @param array $data Data.
      * @return string
      */
-    public function saveItem(array $data)
+    public function saveItemAll(array $data)
     {
-        $validator  = Validator::make($data, $this->rules);
-
-        if ($validator->fails()) {
-            return $validator->errors()->first();
-        }
-
-        // en_name 同平台下不能重复
-        if (!$this->id) {
-            $count = self::where('en_name', '=', $data['en_name'])->where('main_game_plat_code', $data['main_game_plat_code'])->count();
-            if ($count > 0) {
-                return '对不起, 同平台下, 标识(en_name)已经存在!!';
-            }
-        } else {
-            $count = self::where('en_name', '=', $data['en_name'])->where('main_game_plat_code', $data['main_game_plat_code'])->where('id', '<>', $this->id)->count();
-            if ($count > 0) {
-                return '对不起, 同平台下, 标识(en_name)已经存在!!';
+        DB::beginTransaction();
+        foreach ($data as $v) {
+            if (!self::find($v['id'])) {
+                unset($v['deleted_at']);
+                if (!self::insert($v)) {
+                    DB::rollBack();
+                    return 0;
+                }
             }
         }
-
-        $this->main_game_plat_code          = $data['main_game_plat_code'];
-        $this->en_name                      = $data['en_name'];
-        $this->cn_name                      = $data['cn_name'];
-
-        $this->pc_game_code                 = $data['pc_game_code'] ?? '';
-        $this->pc_game_deputy_code          = $data['pc_game_deputy_code'] ?? '';
-        $this->mobile_game_code             = $data['mobile_game_code'] ?? '';
-        $this->mobile_game_deputy_code      = $data['mobile_game_deputy_code'] ?? '';
-
-        $this->record_match_code            = $data['record_match_code'] ?? '';
-        $this->record_match_deputy_code     = $data['record_match_deputy_code'] ?? '';
-
-        $this->img                          = $data['img'] ?? '';
-        $this->type                         = $data['type'] ?? '';
-        $this->category                     = $data['category'] ?? '';
-        $this->line_num                     = $data['line_num'] ?? '';
-
-        $this->able_demo                    = $data['able_demo'] ?? '';
-        $this->able_recommend               = $data['able_recommend'] ?? '';
-        $this->bonus_pool                   = $data['bonus_pool'] ?? '';
-        $this->line_num                     = $data['line_num'] ?? '';
-
-        $this->save();
-
-        return true;
+        DB::commit();
+        return 1;
     }
 
     /**

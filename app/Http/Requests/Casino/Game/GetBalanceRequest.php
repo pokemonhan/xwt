b945de199f0ae32d9ec0f093c8715cc8427530ca
pengpeng <vonly1024@gmail.com>
2019-09-23 15:35:08 +0800

    验证

--- app/Http/Requests/Casino/Game/GetBalanceRequest.php
+++ app/Http/Requests/Casino/Game/GetBalanceRequest.php
@@ -9,14 +9,18 @@ namespace App\Http\Requests\Casino\Game;
 
 use App\Http\Requests\BaseFormRequest;
 
+/**
+ * Class GetBalanceRequest
+ * @package App\Http\Requests\Casino\Game
+ */
 class GetBalanceRequest extends BaseFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
-     * @return bool
+     * @return boolean
      */
-    public function authorize(): bool
+    public function authorize(): boolean
     {
         return true;
     }
@@ -29,8 +33,7 @@ class GetBalanceRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'mainGamePlat' => 'required|string|exists:def_main_game_plats,main_game_plat_code',
-            'accountUserName' => 'required|min:4|max:255|string'
+            'mainGamePlat' => 'required|string',
         ];
     }
 

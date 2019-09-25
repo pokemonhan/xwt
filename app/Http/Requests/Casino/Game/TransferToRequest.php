0d9a49df26ee85fc186f4ec53bc00d9fc254dcb1
pengpeng <vonly1024@gmail.com>
2019-09-25 10:13:54 +0800

    返回值修复

--- app/Http/Requests/Casino/Game/TransferToRequest.php
+++ app/Http/Requests/Casino/Game/TransferToRequest.php
@@ -20,7 +20,7 @@ class TransferToRequest extends BaseFormRequest
      *
      * @return boolean
      */
-    public function authorize(): boolean
+    public function authorize(): bool
     {
         return true;
     }
